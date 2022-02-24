93319e533f12a2cd57d3521d634cf70088e13e2c
HannanSiddiqui <64493764+HannanSiddiqui@users.noreply.github.com>
2022-02-24 14:01:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function onbulb(){
+    var bulb = document.getElementById("bulb")
+    bulb.src = "onBulb.jpg"
+}
+
+function offbulb(){
+    var bulb = document.getElementById("bulb")
+    bulb.src = "offBulb.jpg"
+}
